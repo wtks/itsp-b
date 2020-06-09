be22bd663f1eb14b05ca5938ca075157ea43f31a
K. Nicolas Hayashi <by.yashinomi@gmail.com>
2020-06-09 15:37:20 +0900

    add PBI3

--- main.js
+++ main.js
@@ -28,6 +28,17 @@ const server = http.createServer(function (req, res) {
 					res.end(JSON.stringify(list))
 				})
 			break
+		case "/b":
+			axios.get('http://s2-public-api-prod.us-west-2.elasticbeanstalk.com/v1/paper/0796f6cd7f0403a854d67d525e9b32af3b277331')
+				.then(r => {
+					res.writeHead(200, {"Content-Type": "text/plain"})
+					const list = []
+					r.data.citations.forEach(v => {
+						list.push(v.title)
+					})
+					res.end(JSON.stringify(list))
+				})
+			break
 	}
 }).listen(80);
 
