906eca113248dc218ec3d44eadbb656ce92e0b95
K. Nicolas Hayashi <by.yashinomi@gmail.com>
2020-06-16 16:14:03 +0900

    fix routing

--- main.js
+++ main.js
@@ -5,6 +5,35 @@ const express = require('express');
 
 const app = express();
 app.use(express.static(__dirname + "/frontend/dist/"));
+app.get("/t", function(req, res){
+	axios.get('http://s2-public-api-prod.us-west-2.elasticbeanstalk.com/v1/paper/0796f6cd7f0403a854d67d525e9b32af3b277331')
+		.then(r => {
+			res.writeHead(200, { "Content-Type": "application/json charset=utf-8" })
+			res.end(JSON.stringify(r.data.title))
+		})
+})
+app.get("/a", function(req, res){
+	axios.get('http://s2-public-api-prod.us-west-2.elasticbeanstalk.com/v1/paper/0796f6cd7f0403a854d67d525e9b32af3b277331')
+		.then(r => {
+			res.writeHead(200, {"Content-Type": "application/json charset=utf-8"})
+			const list = []
+			r.data.references.forEach(v => {
+				list.push(v.title)
+			})
+			res.end(JSON.stringify(list))
+		})
+})
+app.get("/b", function(req, res){
+	axios.get('http://s2-public-api-prod.us-west-2.elasticbeanstalk.com/v1/paper/0796f6cd7f0403a854d67d525e9b32af3b277331')
+		.then(r => {
+			res.writeHead(200, {"Content-Type": "application/json charset=utf-8"})
+			const list = []
+			r.data.citations.forEach(v => {
+				list.push(v.title)
+			})
+			res.end(JSON.stringify(list))
+		})
+})
 app.get(/.*/, function (req, res) {
 	res.sendFile(__dirname + "/index.html")
 });
