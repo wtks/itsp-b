a77889eec76c51b91214c8f4a435f5fc53a717db
Durun <negiigainuki@gmail.com>
2020-07-07 14:23:46 +0900

    doi available

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ const cors = require('cors')
 const app = express();
 app.use(cors());
 app.use(express.static(__dirname + "/frontend/dist/"));
-app.get("/t/:paperId", function(req, res){
+app.get("/t/:paperId([^/]*)", function(req, res){
 	const paperId = req.params.paperId
 	axios.get(`http://s2-public-api-prod.us-west-2.elasticbeanstalk.com/v1/paper/${paperId}`)
 		.then(r => {
 			res.status(200).json(r.data)
 		})
 		.catch(error => {
-			res.status(error.response.status)
+			res.status(error.response.status).end()
 		})
 })
 app.listen(8080);
