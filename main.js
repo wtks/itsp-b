ecaffb98022b7027a3a8f2bcacb9583c7970bea8
Durun <negiigainuki@gmail.com>
2020-07-14 15:12:47 +0900

    server returns paper list

--- main.js
+++ main.js
@@ -16,4 +16,39 @@ app.get("/t/:paperId([^/]*)", function(req, res){
 			res.status(error.response.status).end()
 		})
 })
+
+// {title: str, authors: str, doi: str, year: str} のリストを返す
+app.get("/s/:keyword", function (req, res) {
+	const keyword = req.params.keyword
+	const num = 10	// 最大件数
+	axios.get(`https://dblp.org/search/publ/api?format=json&c=0&h=${num}&q=${keyword}`)
+		.then(r => {
+			const hitTotal = r.data.result.hits['@total']
+			let papers
+			if (hitTotal != 0) {
+				papers = r.data.result.hits.hit
+				papers = papers
+					.filter(paper => {
+						const doi = paper.info.doi
+						return doi !== undefined
+					})
+					.map(paper => {
+						const authors = paper.info.authors.author.map(v => v.text)
+						return {
+							title: paper.info.title,
+							authors: authors,
+							doi: paper.info.doi,
+							year: paper.info.year
+						}
+					})
+			}
+			else papers = []
+
+			res.set('Cache-Control', 'max-age=3600')
+			res.status(200).json(papers)
+		})
+		.catch(error => {
+			res.status(error.response.status).end()
+		})
+})
 app.listen(8080);
