01083d4b10c62753f6861f0094a40bb9de6f4fc5
Durun <negiigainuki@gmail.com>
2020-07-21 13:55:48 +0900

    change routing

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,16 +7,14 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    // name: 'Home',
+    // component: Home
+    redirect: { name: 'Graph' }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/graph',
+    name: 'Graph',
+    component: Home
   }
 ]
 
