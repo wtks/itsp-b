51fa57d9f67f4cddd3fa6dbb7e76033cba4815a6
K. Nicolas Hayashi <by.yashinomi@gmail.com>
2020-07-21 15:41:29 +0900

    work in progress

--- frontend/src/views/Results.vue
+++ frontend/src/views/Results.vue
@@ -14,6 +14,28 @@ export default {
     return {
       searchResults: []
     }
+  },
+  watch: {
+    $route: 'initialize'
+  },
+  mounted: async function () {
+    this.initialize()
+  },
+  methods: {
+    search: async function (query) {
+      try {
+        const res = (await axios.get(`http://localhost:8080/s/${query}`))
+        return res.data
+      } catch (e) {
+        console.log(e)
+        return null
+      }
+    },
+    initialize: async function () {
+      if (this.$route.query.q) {
+        this.searchResults = await this.search(this.$route.query.q)
+      }
+    }
   }
 }
 </script>
