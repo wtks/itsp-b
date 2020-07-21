1f12e7463261af07c56ddeab48a9026dd2dd2318
K. Nicolas Hayashi <by.yashinomi@gmail.com>
2020-07-21 16:08:39 +0900

    PBI-14 done

--- frontend/src/views/Results.vue
+++ frontend/src/views/Results.vue
@@ -1,19 +1,28 @@
 <template>
   <div>
+    <search-box />
     <ul>
       <li v-for="result in searchResults" :key="result.doi">
+        <router-link :to="{ name: 'Graph', query: {id: result.doi}}">
         <span class="title">{{ result.title }}</span>,{{ result.authors }},{{ result.year }}. {{ result.doi }}
+        </router-link>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import SearchBox from '../components/SearchBox.vue'
+
 export default {
   data: function () {
     return {
       searchResults: []
     }
+  },
+  components: {
+    SearchBox
   },
   watch: {
     $route: 'initialize'
