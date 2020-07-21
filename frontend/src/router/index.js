8f7827f47ff73f26c98d4b7a9b10c85262a7237f
Durun <negiigainuki@gmail.com>
2020-07-21 15:20:33 +0900

    add Top page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Graph from '../views/Graph.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    // name: 'Home',
-    // component: Home
-    redirect: { name: 'Graph' }
+    name: 'Home',
+    component: Home
   },
   {
     path: '/graph',
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Citemap</h1>
+    <SearchBox/>
+  </div>
+</template>
+
+<script>
+import SearchBox from '../components/SearchBox.vue'
+
+export default {
+  components: {
+    SearchBox
+  }
+}
+</script>
