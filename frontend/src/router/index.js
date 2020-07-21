a3359b8c2b91505f9f497971f90c13d77e7480d7
K. Nicolas Hayashi <by.yashinomi@gmail.com>
2020-07-21 15:40:35 +0900

    fix routing

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Graph from '../views/Graph.vue'
+import Results from '../views/Results.vue'
 
 Vue.use(VueRouter)
 
@@ -14,7 +15,12 @@ const routes = [
   {
     path: '/graph',
     name: 'Graph',
-    component: Home
+    component: Graph
+  },
+  {
+    path: '/search',
+    name: 'SearchResult',
+    component: Results
   }
 ]
 
