81848bc50f65ab947ad5db03ac4210e9ccbce4b3
K. Nicolas Hayashi <by.yashinomi@gmail.com>
2020-07-21 15:52:20 +0900

    fix confilct again

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Citemap</h1>
+    <SearchBox/>
+  </div>
+</template>
+
+<script>
+import SearchBox from '../components/SearchBox.vue'
+export default {
+  components: {
+    SearchBox
+  }
+}
+</script>
